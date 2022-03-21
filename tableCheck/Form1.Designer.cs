2333e940ae351eccd6ac5f5ddf7c3470ba3c989e
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-03-21 22:17:06 +0900

    초기 커밋.

---
+++ tableCheck/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace tableCheck
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.splitter1);
+			this.panel1.Controls.Add(this.dataGridView1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(986, 359);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(40, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "체크";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 32);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 23;
+			this.dataGridView1.Size = new System.Drawing.Size(986, 327);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// splitter1
+			// 
+			this.splitter1.Cursor = System.Windows.Forms.Cursors.HSplit;
+			this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.splitter1.Location = new System.Drawing.Point(0, 29);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(986, 3);
+			this.splitter1.TabIndex = 5;
+			this.splitter1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(986, 359);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "테이블체크";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Splitter splitter1;
+	}
+}
+
---
+++ tableCheck/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace tableCheck
+{
+	/// <summary>
+	/// 테이블 dawoon과 dawoon2 비교하기
+	/// dawoon 테이블2의 컬럼 갯수가 다르면 컬럼추가하기
+	/// </summary>
+
+
+	public partial class Form1 : Form
+	{
+			MySqlConnection connection = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8");
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1_Click(sender, e);
+		}
+		//private void connect(string selectQuery, string account)
+		//{
+		//	selectQuery = "SELECT * FROM dawoon.dc_items where flagYN = 'Y';";
+		//	connection.Open();
+		//	MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+		//	MySqlDataReader reader = cmd.ExecuteReader();
+		//	while (reader.Read())
+		//	{
+		//	}
+		//	connection.Close();
+		//}
+
+		//		QuerySearch = "SELECT EXISTS ( SELECT 1 FROM Information_schema.tables WHERE table_schema = 'dawoon' AND TABLE_NAME = 'dc_account') AS 테이블확인";
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (MySqlConnection connection = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8"))
+			{
+				try//예외 처리
+				{
+					connection.Open();
+					string strSql = "SHOW TABLES IN dawoon;";
+					MySqlCommand cmd = new MySqlCommand(strSql, connection);
+
+					MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+					//  MySqlCommandBuilder _bd = new MySqlCommandBuilder(da);
+					DataTable dt = new DataTable();
+					da.Fill(dt);
+
+			
+
+
+					dataGridView1.DataSource = dt;
+					connection.Close();
+
+
+					dataGridView1.ReadOnly = true;
+					dataGridView1.RowHeadersVisible = false;
+					//dataGridViewMessage.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+					//dataGridViewMessage.Columns[dataGridViewMessage.ColumnCount - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+					//dataGridViewMessage.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+					dataGridView1.Columns[0].Width = 280;
+					dt.Columns.Add("필드수");
+					dt.Columns.Add("progressbar");
+					dt.Columns.Add("상태");
+
+					connection.Open();
+					for (int i = 0; i < dataGridView1.Rows.Count; i++)
+					{
+						string fieldSql = "SELECT COUNT(*) cnt	FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + dataGridView1.Rows[i].Cells[0].Value +"';";
+						MySqlCommand cmd2 = new MySqlCommand(fieldSql, connection);
+						MySqlDataReader rdr = cmd2.ExecuteReader();
+						while (rdr.Read())
+						{
+							dataGridView1.Rows[i].Cells[1].Value = rdr["cnt"].ToString();
+						}
+						rdr.Close();
+					}
+
+					connection.Close();
+
+					//SELECT COUNT(*)	FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'account_book';
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.ToString(),"실패");
+				}
+
+			}
+		}
+
+
+
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+	
+	}
+}
+/*
+ * 
+ * 
+ * 	//ExecuteReader를 이용하여
+					//연결 모드로 데이타 가져오기
+					MySqlCommand cmd = new MySqlCommand(sql, connection);
+					MySqlDataReader table = cmd.ExecuteReader();
+					MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+					DataTable dt = new DataTable();
+					da.Fill(dt);
+					dataGridView1.DataSource = dt;
+					connection.Close();
+					//while (table.Read())
+					//{
+					//	textBox3.Text = table["table_name"].ToString();
+					//	Console.WriteLine(table["table_name"].ToString());
+					//}
+					//table.Close();
+ */
